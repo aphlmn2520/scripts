749d2442e304ea1672a51b9f716dc8b9c7ea0526
aphlmn2520 <52954744+aphlmn2520@users.noreply.github.com>
2021-09-20 16:45:53 +0200

    Create docker-install-fedora.sh

---
+++ docker-install-fedora.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -ex
+
+sudo dnf update -y
+
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+                  
+sudo dnf -y install dnf-plugins-core
+
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+   
+
+sudo dnf install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+newgrp docker 
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo curl \
+    -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose \
+    -o /etc/bash_completion.d/docker-compose
+    
+echo "After reloading your terminal, changes will take effect."    
+ 
